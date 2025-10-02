0f4d6eda7051b53b7d99e7ef0d554bd01565f29c
Lorenzo Cristianini de Oliveira <lorenzo.cristianini.oliveira@escola.pr.gov.br>
2025-10-02 08:17:26 -0300

    Fix output message for odd number check

--- par-impar.py
+++ par-impar.py
@@ -1,6 +1,7 @@
-print(f"Aqui descobriremos se seu número é par ou ímpar!")
+print(f"Aqui descobriremos se seu número é par ou impar!")
 numero = int(input("Digite seu número: "))
 if numero % 2 == 0:
     print(f"O {numero} é par")
 else:
-    print(f"O {numero} é ímpar")
+
+    print(f"O {numero} é impar")
