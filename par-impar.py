d5457da16d7fa48aff9001ed2e67c078fce4f8a3
Lorenzo Cristianini de Oliveira <lorenzo.cristianini.oliveira@escola.pr.gov.br>
2025-10-02 08:15:16 -0300

    Add files via upload

---
+++ par-impar.py
@@ -0,0 +1,6 @@
+print(f"Aqui descobriremos se seu número é par ou ímpar!")
+numero = int(input("Digite seu número: "))
+if numero % 2 == 0:
+    print(f"O {numero} é par")
+else:
+    print(f"O {numero} é ímpar")
